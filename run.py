6dd03f4dcad9cec291d9549c70035f77334fa623
Pragadeeswaran Sathyanarayanan <psathyan@redhat.com>
2021-09-30 20:49:48 +0530

    Adding support for console and file logging
    Signed-off-by: Pragadeeswaran Sathyanarayanan <psathyan@redhat.com>

--- run.py
+++ run.py
@@ -1,30 +1,36 @@
 """Entry point for Red Hat Ceph QE CI."""
 import logging
 import sys
-import yaml
-from docopt import docopt
 from pathlib import Path
 from typing import Dict
 
-from ceph.utils import cleanup_ceph_nodes
-from utility.utils import generate_unique_id, add_log_file_handler, merge_dicts
+import yaml
+from docopt import docopt
+
+from utils.log import (
+    LOG_FORMAT,
+    add_log_file_handler,
+    close_log_handlers,
+    create_or_get_root_log_dir,
+)
+from utils.utils import generate_unique_id, merge_dicts
 
 usage = """
 An orchestrator that calls the relevant methods based on the provided workflow.
 
 Usage:
-  run.py -h | --help
-  run.py --cleanup <instance-prefix> (--conf <conf-file)... [--verbose]
+  run.py -h | --help 
   run.py (--conf <conf-file>)...
     [--rhcs <rhcs-version>]
     [--build <build-type>]
     [--clusters-spec <cluster-spec>]
     [--test-suite <test-suite>]...
     [--verbose]
+    [--cleanup <instances-prefix>]
 
 Options:
   -h --help                         Displays the usage and supported options.
-  --cleanup <instance-prefix>       Runs the VM environment cleanup.
+  --cleanup <instances-prefix>      Runs the VM environment cleanup.
   --conf <rhcs-version>             Configuration files in YAML format containing
                                     details for execution.
   --rhcs <rhcs-version>             Red Hat Ceph Storage version
@@ -34,72 +40,79 @@ Options:
   --test-suite <test-suite>         Absolute test suite file. Supports multiple values
   --verbose                         Increases the log level. Default, log level is info
 """
-
-# Configuring the logging options
 LOG = logging.getLogger(__name__)
 logging.basicConfig(
-    handlers=[logging.StreamHandler(sys.stdout)],
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s"
+    handlers=[logging.StreamHandler(sys.stdout)], level=logging.DEBUG, format=LOG_FORMAT
 )
 
 
-def run(cli_args: Dict) -> None:
+def run(conf: Dict, cli_args: Dict) -> None:
     """
     Wrapper method that triggers the workflows based on the provided arguments.
 
     Args:
-        cli_args (Dict)     CLI arguments passed to run.py. Refer cepchi.yaml.template
+        conf (dict):        Single configuration dictionary passed to the module.
+        cli_args (dict)     CLI arguments passed to run.py. Refer cepchi.yaml.template
                             for supported key-value pairs
     Returns:
         None
     Raises:
         Exception
     """
-    # run_id is a unique identifier for the execution run
-    run_id = generate_unique_id(length=6)
-
-    log_level = logging.INFO
-    if cli_args.get("--verbose"):
-        LOG.setLevel(logging.DEBUG)
-        log_level = logging.DEBUG
-        LOG.debug("Log level now is at DEBUG.")
-
-    add_log_file_handler(run_id, "startup.log", log_level)
-
-    # Read test configuration and CLI arguments
-    configs = dict()
-
-    for file_ in cli_args["--conf"]:
-        file_ = Path(file_).absolute()
-        with file_.open('r') as fh:
-            config = yaml.safe_load(fh)
-            merge_dicts(config, configs)
-
     # Always check if the operation is cleanup first before proceeding with workflow
     if cli_args.get("--cleanup"):
-        cleanup_ceph_nodes(configs["compute"]["credential"], cli_args["--cleanup"])
+        # cleanup_ceph_nodes(configs["compute"]["credential"], cli_args["--cleanup"])
+        print("Nothing to do")
         return
 
     # CLI args must have the highest precedence
     if cli_args["--rhcs"]:
-        configs["rhcs"] = cli_args["--rhcs"]
+        conf["rhcs"] = cli_args["--rhcs"]
 
     if cli_args["--build"]:
-        configs["build"] = cli_args["--build"]
+        conf["build"] = cli_args["--build"]
 
     if cli_args["--clusters-spec"]:
-        configs["compute"]["spec"] = cli_args["--clusters-spec"]
+        conf["compute"]["spec"] = cli_args["--clusters-spec"]
 
     if cli_args["--test-suite"]:
-        configs["test_suites"] = cli_args["--test-suite"]
+        conf["test_suites"] = cli_args["--test-suite"]
 
 
 if __name__ == "__main__":
     args = docopt(usage)
     try:
+        # run_id is a unique identifier for the execution run
+        run_id = generate_unique_id(length=6)
+
+        # Read test configuration and CLI arguments
+        configs = dict()
+
+        for file_ in args["--conf"]:
+            file_ = Path(file_).absolute()
+            with file_.open("r") as fh:
+                config = yaml.safe_load(fh)
+                merge_dicts(config, configs)
+
+        # Initiate the loggers
+        msg = """
+        Note :
+            By default, the logging directory is '/ceph/cephci-jenkins' if it exists.
+            Otherwise, it is '/tmp' on the local system.
+        """
+        print(msg)
+
+        log_level = logging.DEBUG if args.get("--verbose") else logging.INFO
+        create_or_get_root_log_dir(run_id, config.get("log", {}).get("path"))
+        add_log_file_handler(run_id, "startup.log", log_level)
+
+        LOG.info("Testing from info level")
+        LOG.debug("Testing from debug level")
+
         LOG.debug(args)
-        run(args)
+        run(configs, args)
     except BaseException as be:  # no-qa
         LOG.error(be)
         sys.exit(1)
+    finally:
+        close_log_handlers()
