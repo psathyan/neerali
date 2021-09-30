2b6799253ec86412c96d13dd07df1393a0efc045
Pragadeeswaran Sathyanarayanan <psathyan@redhat.com>
2021-09-30 20:50:56 +0530

    Adding support for generic methods in utils
    Signed-off-by: Pragadeeswaran Sathyanarayanan <psathyan@redhat.com>

--- utils/utils.py
+++ utils/utils.py
@@ -1,47 +1,10 @@
 """Module contains use workflows for the core package."""
-from pathlib import Path
-from typing import Dict, Optional
+import logging
+import random
+from string import ascii_lowercase, digits
+from typing import Dict
 
-
-def create_run_dir(run_id: str, log_dir="/tmp") -> str:
-    """
-    Create the root logging folder for the run.
-
-    Args:
-        run_id: id of the test run. used to name the directory
-        log_dir: log directory. default: "/tmp"
-
-    Returns:
-        Full path of the created directory
-    """
-    msg = """\nNote :
-    1. Custom log directory will be disabled if '/ceph/cephci-jenkins' exists.
-    2. If custom log directory not specified, then '/tmp' directory is considered .
-    """
-    print(msg)
-
-    root_log_dir = Path("/ceph/cephci-jenkins")
-
-    if not root_log_dir.exists():
-        root_log_dir = Path("/tmp")
-
-    dir_name = f"cephci-run-{run_id}"
-    base_dir = "/ceph/cephci-jenkins"
-
-    if
-
-    if not os.path.isdir(base_dir):
-        if not os.path.isabs(log_dir):
-            log_dir = os.path.join(os.getcwd(), log_dir)
-        base_dir = log_dir
-    run_dir = os.path.join(base_dir, dir_name)
-    try:
-        os.makedirs(run_dir)
-    except OSError:
-        if "jenkins" in getpass.getuser():
-            raise
-
-    return run_dir
+LOG = logging.getLogger(__name__)
 
 
 def merge_dicts(source: Dict, target: Dict) -> None:
@@ -64,3 +27,16 @@ def merge_dicts(source: Dict, target: Dict) -> None:
             continue
 
         target.update({key: value})
+
+
+def generate_unique_id(length: int = 1) -> str:
+    """
+    Returns a unique string of length requested in arguments.
+
+    Args:
+        length (int): Length of the string
+
+    Returns:
+        (str) a unique string
+    """
+    return "".join(random.choices(ascii_lowercase + digits, k=length))
