9cf04c874607322ae2c2eb2845d1587fe84a88dd
Arunkumar Ravindranath <arunkumar.ravindranath@ibm.com>
2024-11-08 09:04:38 +0000

    Add the following new switches:
     * `--iodepth` : iodepth for fio, default is `128`,
     * `--rw`      : Operation type, allowed values are `read`, `write`, `randread`, `randwrite`. default is `randwrite`
     * `--bs`      : block size, default is `4k`
     * `--format`  : fio output format. allowed values are `normal`, `json`, `json+`, `terse`. default is `json`
     * `--fio-file`: provide fio file instead of all above arguments. Looks for the file under `/fio_files` and needs to be passed as container volume
                     eg: `podman run --privileged -v $PWD:/fio_files neerali_fio --fio-file my_fio_file`

--- roles/fio/files/runfio.sh
+++ roles/fio/files/runfio.sh
@@ -1,45 +1,71 @@
 #!/bin/bash
 set -euo pipefail
 
-LONGOPTS=block-device:,time-to-run:,num-jobs:,dev-type,file-size
-OPTIONS=b:t:n:d:s
+LONGOPTS=fio-file:,block-device:,time-to-run:,num-jobs:,dev-type:,file-size:,iodepth:,format:,rw:,bs:
+OPTIONS=f:b:t:n:d:s
 
+format=json
+fio_file="null"
+fio_file_dir="/fio_files"
 devtype=fs
 filesize="1G"
 numjobs=1
 timetorun=0
+iodepth=128
+bs=4k
+rw=randwrite
 device=""
 
 _args=$(getopt --options=${OPTIONS} --longoptions=${LONGOPTS} --name "$@" -- "$@")
 while [ "$#" -gt 0 ]; do
   case "$1" in
+    -f|--fio-file)
+	      fio_file=$2
+	      shift 2
+        ;;
     -b|--block-device)
-      device=$2
-      shift 2
-      ;;
+        device=$2
+        shift 2
+        ;;
     -t|--time-to-run)
-      timetorun=$2
-      shift 2
-      ;;
-	-n|--num-jobs)
-      numjobs=$2
-      shift 2
-      ;;
-	-d|--dev-type)
-      devtype=$2
-      shift 2
-      ;;
-	-s|--file-size)
-	  filesize=$2
-	  shift 2
-	  ;;
+        timetorun=$2
+        shift 2
+        ;;
+    -n|--num-jobs)
+        numjobs=$2
+        shift 2
+        ;;
+    --format)
+        format=$2
+        shift 2
+	      ;;
+    -d|--dev-type)
+        devtype=$2
+        shift 2
+        ;;
+    -s|--file-size)
+        filesize=$2
+        shift 2
+        ;;
+    --iodepth)
+        iodepth=$2
+        shift 2
+        ;;
+    --bs)
+        bs=$2
+        shift 2
+        ;;
+    --rw)
+        rw=$2
+        shift 2
+          ;;
     --)
-	  shift 2
-      break
-      ;;
-	*)
-      echo "Unknown option"
-      exit 3
+        shift 2
+        break
+        ;;
+    *)
+        echo "Unknown option"
+        exit 3
   esac
 done
 
@@ -48,17 +74,17 @@ FIO_CMD="fio"
 FIO_ARGS="--group_reporting --direct=1 --ioengine=libaio"
 
 if [[ $timetorun -gt 0 ]]; then
-	FIO_ARGS="$FIO_ARGS --time_based --runtime=$timetorun"
+    FIO_ARGS="$FIO_ARGS --time_based --runtime=$timetorun"
 fi
-if [[ ${devtype} == "blk" ]]; then
-	FIO_ARGS="$FIO_ARGS --name=seq_write_test --filename=$device --rw=write --bs=1M --numjobs=$numjobs"
+if [[ ${fio_file} != "null" ]]; then
+    FIO_ARGS="${fio_file_dir}/${fio_file}"
+elif [[ ${devtype} == "blk" ]]; then
+    FIO_ARGS="$FIO_ARGS --name=neerali_${devtype}_test --filename=$device --rw=$rw --bs=$bs --numjobs=$numjobs --iodepth=$iodepth"
 elif [[ ${devtype} == "fs" ]]; then
-	FIO_ARGS="$FIO_ARGS --name=nfs_test --directory=/data --rw=randwrite --bs=4k --size=$filesize --numjobs=$numjobs"
-else 
-	echo "Invalid Arguments"
-	exit 1
+    FIO_ARGS="$FIO_ARGS --name==neerali_${devtype}_test --directory=/data --rw=$rw --bs=$bs --size=$filesize --numjobs=$numjobs --iodepth=$iodepth"
+else
+    echo "Invalid Arguments"
+    exit 1
 fi
 
-echo Running: $FIO_CMD $FIO_ARGS
-
-$FIO_CMD $FIO_ARGS
+$FIO_CMD $FIO_ARGS --output-format=$format
